86770c737ca749f0ee6bd9fdd55fb98c3625d7fe
RickyFirmansyah27 <ricky_firmansyah@teknokrat.ac.id>
2025-05-19 14:26:07 +0700

    add auth service and generate auth response

--- lib/helpers/parse_response_helper.dart
+++ lib/helpers/parse_response_helper.dart
@@ -19,3 +19,17 @@ List<Map<String, dynamic>> generateSkinResponse(dynamic response) {
   }
   return <Map<String, dynamic>>[];
 }
+
+List<Map<String, dynamic>> generateAuthResponse(dynamic response) {
+  if (response is Map<String, dynamic> &&
+      response['data'] is Map<String, dynamic> &&
+      response['data']['user'] is List &&
+      response['data']['token'] != null) {
+    final List<Map<String, dynamic>> userResponse = (response['data']['user'] as List)
+        .cast<Map<String, dynamic>>()
+        .toList();
+    userResponse[0]['token'] = response['data']['token'];
+    return userResponse;
+  }
+  return <Map<String, dynamic>>[];
+}
---
+++ lib/services/auth_service.dart
@@ -0,0 +1,29 @@
+import 'package:lynxgaming/config/api_client.dart';
+import 'package:lynxgaming/helpers/logger.dart';
+import 'package:lynxgaming/helpers/parse_response_helper.dart';
+
+Future<List<Map<String, dynamic>>> authLogin({
+  Map<String, dynamic> bodyRequest = const {},
+}) async {
+
+  try {
+    final response = await ApiClient.apiPost(
+      url: '/v1/login',
+      body: bodyRequest,
+    );
+
+    logger.d('Login API response: $response');
+    final auth = generateAuthResponse(response);
+
+    return auth;
+  } catch (e, stackTrace) {
+   
+    logger.e(
+      'Unexpected error login',
+      error: e,
+      stackTrace: stackTrace,
+    );
+
+    return <Map<String, dynamic>>[];
+  }
+}
